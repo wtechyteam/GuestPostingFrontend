a4a7d3b7a58ee89f4514b638674eeccf042ca08e
WTechy <wtechy@gmail.com>
2024-09-13 17:39:12 +0530

    prettier

--- app/common/Sidebar1.jsx
+++ app/common/Sidebar1.jsx
@@ -10,25 +10,22 @@ import {
   sidebarClasses,
 } from "react-pro-sidebar";
 import Image from "next/image";
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import { useRouter } from 'next/navigation';
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 export default function Sidebar1({ ...props }) {
   const router = useRouter();
   const handleLogout = async () => {
     try {
-      // Make a request to the logout API
-      await axios.post('http://localhost:3001/api/logout');
-  
-      // Clear cookies related to authentication
-      Cookies.remove('authToken');
-      Cookies.remove('fullName');
-  
-      // Redirect to the login page or home page
-      router.push('/login');
+      await axios.post("http://localhost:3001/api/logout");
+
+      Cookies.remove("authToken");
+      Cookies.remove("fullName");
+
+      router.push("/login");
     } catch (error) {
-      console.error('Error during logout:', error);
+      console.error("Error during logout:", error);
     }
   };
 
@@ -39,7 +36,6 @@ export default function Sidebar1({ ...props }) {
       rootStyles={{ [`${sidebarClasses.container}`]: { gap: 6 } }}
       className={`${props.className} flex flex-col h-screen gap-1.5 bg-gray-10 fixed top-0 left-0 overflow-auto`}
     >
-     
       <Heading size="label_regular" as="p" className="ml-6 !text-gray-400">
         Dashboard
       </Heading>
@@ -64,135 +60,141 @@ export default function Sidebar1({ ...props }) {
       >
         <div className="flex flex-col gap-1">
           <Link href="/dashboardBuyer">
-          <MenuItem
-            icon={
-              <Image
-                src="/images/HouseLine.png"
-                width={20}
-                height={20}
-                alt="Home Icon"
-                className="h-[20px] w-[20px]"
-              />
-            }
-                                                                                                                                                                                                            
-          >
-            Dashboard
-          </MenuItem>
+            <MenuItem
+              icon={
+                <Image
+                  src="/images/HouseLine.png"
+                  width={20}
+                  height={20}
+                  alt="Home Icon"
+                  className="h-[20px] w-[20px]"
+                />
+              }
+            >
+              Dashboard
+            </MenuItem>
           </Link>
           <Link href="/profileDetails">
-          <MenuItem
-            icon={
-              <Image
-                src="/images/User.png"
-                width={20}
-                height={20}
-                alt="Wishlist Icon"
-                className="h-[20px] w-[20px]"
-              />
-            }
-          >
-            Profile Details
-          </MenuItem>
+            <MenuItem
+              icon={
+                <Image
+                  src="/images/User.png"
+                  width={20}
+                  height={20}
+                  alt="Wishlist Icon"
+                  className="h-[20px] w-[20px]"
+                />
+              }
+            >
+              Profile Details
+            </MenuItem>
           </Link>
           <Link href="/orderManagement">
-          <SubMenu
-            icon={
-              <Image
-                src="/images/FolderNotchOpen.png"
-                width={20}
-                height={20}
-                alt="Folder Icon"
-                className="h-[20px] w-[20px]"
-              />
-            }
-            label="Order Management"
-          >
-            <Link href="/orderManagement"> <MenuItem className="pl-4">Order History </MenuItem></Link>
-            <Link href="/orderManagement"> <MenuItem className="pl-4">Order Status </MenuItem></Link>
-            {/* <Link href="/orderManagement"> <MenuItem className="pl-4">Order List </MenuItem></Link> */}
-          </SubMenu>
+            <SubMenu
+              icon={
+                <Image
+                  src="/images/FolderNotchOpen.png"
+                  width={20}
+                  height={20}
+                  alt="Folder Icon"
+                  className="h-[20px] w-[20px]"
+                />
+              }
+              label="Order Management"
+            >
+              <Link href="/orderManagement">
+                {" "}
+                <MenuItem className="pl-4">Order History </MenuItem>
+              </Link>
+              <Link href="/orderManagement">
+                {" "}
+                <MenuItem className="pl-4">Order Status </MenuItem>
+              </Link>
+              {/* <Link href="/orderManagement"> <MenuItem className="pl-4">Order List </MenuItem></Link> */}
+            </SubMenu>
           </Link>
           <Link href="/paymentInfo">
-          <SubMenu
-            icon={
-              <Image
-                src="/images/CreditCard.png"
-                width={20}
-                height={20}
-                alt="Credit Card Icon"
-                className="h-[20px] w-[20px]"
-              />
-            }
-            label="Payment Information"
-          >
-           <Link href="/paymentInfo"> <MenuItem className="pl-4">Transaction History </MenuItem></Link>
-           {/* <Link href="/paymentInfo"> <MenuItem className="pl-4">Billing Information </MenuItem></Link> */}
-           <Link href="/paymentInfo"><MenuItem className="pl-4">Payment Method </MenuItem></Link>
-          </SubMenu>
+            <SubMenu
+              icon={
+                <Image
+                  src="/images/CreditCard.png"
+                  width={20}
+                  height={20}
+                  alt="Credit Card Icon"
+                  className="h-[20px] w-[20px]"
+                />
+              }
+              label="Payment Information"
+            >
+              <Link href="/paymentInfo">
+                {" "}
+                <MenuItem className="pl-4">Transaction History </MenuItem>
+              </Link>
+              <Link href="/paymentInfo">
+                <MenuItem className="pl-4">Payment Method </MenuItem>
+              </Link>
+            </SubMenu>
           </Link>
           <Link href="/wishlist">
-          <MenuItem
-            icon={
-              <Image
-                src="/images/Cards.png"
-                width={20}
-                height={20}
-                alt="Wishlist Icon"
-                className="h-[20px] w-[20px]"
-              />
-            }
-          >
-            Wishlist
-          </MenuItem>
+            <MenuItem
+              icon={
+                <Image
+                  src="/images/Cards.png"
+                  width={20}
+                  height={20}
+                  alt="Wishlist Icon"
+                  className="h-[20px] w-[20px]"
+                />
+              }
+            >
+              Wishlist
+            </MenuItem>
           </Link>
           <Link href="/notifications">
-          <MenuItem
-            icon={
-              <Image
-                src="/images/ClipboardText.png"
-                width={20}
-                height={20}
-                alt="Wishlist Icon"
-                className="h-[20px] w-[20px]"
-              />
-            }
-           
-          >
-         Notifications
-          
-          </MenuItem>
+            <MenuItem
+              icon={
+                <Image
+                  src="/images/ClipboardText.png"
+                  width={20}
+                  height={20}
+                  alt="Wishlist Icon"
+                  className="h-[20px] w-[20px]"
+                />
+              }
+            >
+              Notifications
+            </MenuItem>
           </Link>
           <Link href="/support">
-          <MenuItem
-            icon={
-              <Image
-                src="/images/Question.png"
-                width={20}
-                height={20}
-                alt="Wishlist Icon"
-                className="h-[20px] w-[20px]"
-              />
-            }
-           
-          >
-        Support
-            {/* <Link href="/notifications"><MenuItem className="pl-4">Order Updates </MenuItem></Link> */}
-          </MenuItem>
+            <MenuItem
+              icon={
+                <Image
+                  src="/images/Question.png"
+                  width={20}
+                  height={20}
+                  alt="Wishlist Icon"
+                  className="h-[20px] w-[20px]"
+                />
+              }
+            >
+              Support
+              {/* <Link href="/notifications"><MenuItem className="pl-4">Order Updates </MenuItem></Link> */}
+            </MenuItem>
           </Link>
           <Link href="/settings">
-          <MenuItem
-            icon={
-              <Image
-                src="/images/PuzzlePiece.png"
-                width={20}
-                height={20}
-                alt="Wishlist Icon"
-                className="h-[20px] w-[20px]"
-              />
-            }
-          >
-            Settings
-          </MenuItem>
+            <MenuItem
+              icon={
+                <Image
+                  src="/images/PuzzlePiece.png"
+                  width={20}
+                  height={20}
+                  alt="Wishlist Icon"
+                  className="h-[20px] w-[20px]"
+                />
+              }
+            >
+              Settings
+            </MenuItem>
           </Link>
         </div>
 
