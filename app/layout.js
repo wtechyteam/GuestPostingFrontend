f3ebbfbfbd2cf4a2f779d0070fc85fb2504159e1
Wtechy <randeep@wtmails.com>
2024-10-07 13:22:41 +0530

    aa

--- app/layout.js
+++ app/layout.js
@@ -25,8 +25,10 @@ function RootLayout({ children }) {
           content="width=device-width, initial-scale=1.0"
         />
         <meta name="theme-color" content="#000000" />
+        
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="noindex"/>
       </head>
       <body>{children}</body>
     </html>
