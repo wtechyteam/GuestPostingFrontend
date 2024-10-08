5dcd9a41391b1e2f558a7b844fe0f8152ed2703f
WTechy <wtechy@gmail.com>
2024-10-08 17:35:55 +0530

    keyBenefits section Responsive

--- app/common/BuyerInfoSection.jsx
+++ app/common/BuyerInfoSection.jsx
@@ -9,40 +9,40 @@ export default function BuyerInfoSection({ ...props }) {
   return (
     <div
       {...props}
-      className={`flex items-center w-full md:w-[80%] relative  !text-adsy_com-black
+      className={`flex flex-row md:flex-col items-center w-full md:w-[80%] relative !text-adsy_com-black
                 hover:border-2 hover:border-[#1C817D] hover:rounded-2xl 
                 hover:bg-blue-100 p-4`}
       style={{ borderColor: "transparent" }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className="flex w-full flex-row justify-between items-center">
-        <div className="flex w-1/2 flex-col items-center space-y-12">
-          <h2 className="text-[36px] font-bold">Buyer</h2>
-          <ul className="list-disc pl-6 space-y-4 text-[26px]">
-            <li>Place quality blog posts from buyers</li>
-            <li>Make money writing your blog</li>
-            <li>Get regular payouts, prompt support</li>
-          </ul>
-          <Link href="/signup">
-            <Button
-              size="md"
-              className="min-w-[220px] ml-3.5 font-medium bg-blue-500 text-white transition-colors duration-300"
-            >
-              List Your Website
-            </Button>
-          </Link>
-        </div>
-        <div className="w-1/2 flex justify-end">
+<div className="flex w-full md:w-full flex-col items-center space-y-12 sm:justify-left sm:ml-12 md:ml-8">
+
+  <h2 className="text-[36px] md:text-[36px] font-bold">Buyer</h2>
+  <ul className="list-disc pl-6 space-y-4 text-[26px] md:text-[22px] sm:text-[16px]">
+    <li>Place quality blog posts from buyers</li>
+    <li>Make money writing your blog</li>
+    <li>Get regular payouts, prompt support</li>
+  </ul>
+  <Link href="/signup">
+    <Button
+      size="md"
+      className="min-w-[220px] ml-3.5 font-medium bg-blue-500 text-white transition-colors duration-300"
+    >
+      List Your Website
+    </Button>
+  </Link>
+</div>
+
+      <div className="w-full md:full flex justify-end mt-6 md:mt-5 sm:justify-left sm:ml-12 md:ml-8">
         <video
-            src="/images/videoupwork.mp4"
-            alt="Buyer Info Video"
-            className="w-[494px] h-[300px] object-cover rounded-lg"
-            autoPlay
-            muted
-            loop
-          />
-        </div>
+          src="/images/videoupwork.mp4"
+          alt="Buyer Info Video"
+          className="w-full md:w-[494px] h-[300px] object-cover rounded-lg"
+          autoPlay
+          muted
+          loop
+        />
       </div>
     </div>
   );
--- app/common/SellerInfoSection.jsx
+++ app/common/SellerInfoSection.jsx
@@ -3,46 +3,44 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { Button } from "./Button";
 
-export default function BuyerInfoSection({ ...props }) {
+export default function SellerInfoSection({ ...props }) {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div
       {...props}
-      className={`flex items-center w-full md:w-[80%] relative  !text-adsy_com-black
+      className={`flex md:flex-col-reverse flex-row md:flex-col items-center w-full md:w-[80%] relative !text-adsy_com-black
                 hover:border-2 hover:border-[#1C817D] hover:rounded-2xl 
                 hover:bg-blue-100 p-4`}
       style={{ borderColor: "transparent" }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className="flex w-full flex-row justify-between items-center">
-        <div className="w-1/2 flex justify-end">
-          <video
-            src="/images/videoupwork.mp4"
-            alt="Buyer Info Video"
-            className="w-[494px] h-[300px] object-cover rounded-lg"
-            autoPlay
-            muted
-            loop
-          />
-        </div>
-        <div className="flex w-1/2 flex-col items-center space-y-12">
-          <h2 className="text-[36px] font-bold">Seller</h2>
-          <ul className="list-disc pl-6 space-y-4 text-[26px]">
-            <li>Place quality blog posts from sellers</li>
-            <li>Make money writing your blog</li>
-            <li>Get regular payouts, prompt support</li>
-          </ul>
-          <Link href="/signup">
-            <Button
-              size="md"
-              className=" min-w-[220px] ml-3.5 font-medium bg-blue-500 text-white transition-colors duration-300"
-            >
-              List Your Website
-            </Button>
-          </Link>
-        </div>
+      <div className="md:w-full  flex justify-end mb-4 md:mb-2 sm:ml-12 md:ml-8">
+        <video
+          src="/images/videoupwork.mp4"
+          alt="Buyer Info Video"
+          className="w-[494px] h-[300px] object-cover rounded-lg"
+          autoPlay
+          muted
+          loop
+        />
+      </div>
+      <div className="md:w-full flex flex-col items-center space-y-12 sm:ml-12 md:ml-8">
+        <h2 className="text-[36px] font-bold ">Seller</h2>
+        <ul className="list-disc pl-6 space-y-4 text-[26px] md:text-[22px] sm:text-[16px]">
+          <li>Place quality blog posts from sellers</li>
+          <li>Make money writing your blog</li>
+          <li>Get regular payouts, prompt support</li>
+        </ul>
+        <Link href="/signup">
+          <Button
+            size="md"
+            className="min-w-[220px] ml-3.5 font-medium bg-blue-500 text-white transition-colors duration-300 md:mb-[1rem]"
+          >
+            List Your Website
+          </Button>
+        </Link>
       </div>
     </div>
   );
