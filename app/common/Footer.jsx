d9b27d0791061ef28776fa7da4db7779c08a9cc1
WTechy <wtechy@gmail.com>
2024-10-08 16:05:12 +0530

    footer responsiveness check

--- app/common/Footer.jsx
+++ app/common/Footer.jsx
@@ -1,4 +1,3 @@
-"use client";
 import Image from "next/image";
 import { Input } from "./Input";
 import { Text } from "./Text";
@@ -9,12 +8,12 @@ export default function Footer(props) {
   return (
     <footer
       {...props}
-      className={`${props.className} flex flex-col mt-[54px] py-[102px] md:py-5 bg-gray-10`}
+      className={`${props.className} flex flex-col mt-[54px] py-[102px] md:py-5 lg:py-[102px] bg-gray-10`}
     >
-      <div className="container-xs sm:gap-[27px] gap-[54px] mb-1.5 flex flex-col items-center px-14 md:px-5">
-        <div className="w-[96%] flex items-center justify-between gap-5 md:w-full md-flex-col">
-          <div className="w-[54%] mb-1.5 flex items-start justify-between gap-5 self-end md:w-full md-flex-col md:self-auto">
-            <div className="w-[48%] flex flex-col items-start gap-3.5 self-center md:w-full">
+      <div className="container-xs sm:gap-[27px] gap-[54px] mb-1.5 flex flex-col items-center px-14 md:px-5 lg:px-14">
+        <div className="w-[96%] flex items-center justify-between gap-5 md:w-full lg:w-[96%] md-flex-col">
+          <div className="w-[54%] mb-1.5 flex items-start justify-between gap-5 self-end md:w-full md-flex-col md:self-auto lg:w-[54%]">
+            <div className="w-[48%] flex flex-col items-start gap-3.5 self-center md:w-full lg:w-[48%]">
               <Text
                 size="text2xl"
                 as="p"
@@ -93,7 +92,7 @@ export default function Footer(props) {
                 </li>
               </ul>
             </div>
-            <div className="w-[38%] flex flex-col items-start gap-3.5 md:w-full">
+            <div className="w-[38%] flex flex-col items-start gap-3.5 md:w-full lg:w-[38%]">
               <Text
                 size="text2xl"
                 as="p"
@@ -139,7 +138,7 @@ export default function Footer(props) {
                 </li>
               </ul>
             </div>
-            <div className="flex flex-col items-start gap-3.5">
+            <div className="flex flex-col items-start gap-3.5 lg:w-[14%] md:w-full">
               <Text
                 size="text2xl"
                 as="p"
@@ -202,7 +201,9 @@ export default function Footer(props) {
             </div>
           </div>
 
-          <div className="rounded-[30px] py-[30px] w-[30%] flex flex-col items-start justify-center gap-5 bg-indigo-800_0c px-11 md:w-full md:px-5 sm:p-5">
+          <div
+            className={`rounded-[30px] py-[30px] w-[30%] flex flex-col items-start justify-center gap-5 bg-indigo-800_0c px-11 md:w-full md:justify-center md:items-center lg:w-[30%] lg:justify-start lg:items-start md:px-5 sm:p-5`}
+          >
             <Text
               size="textlg"
               as="p"
@@ -239,9 +240,9 @@ export default function Footer(props) {
             </Text>
           </div>
         </div>
-        <div className="w-[96%] flex flex-col items-end gap-9 md:w-full">
+        <div className="w-[96%] flex flex-col items-end gap-9 md:w-full lg:w-[96%]">
           <div className="border-[0.505px] h-[1.01px] w-full self-stretch border-solid border-dark-3 bg-dark-3" />
-          <div className="w-[60%] flex items-start justify-between gap-5 md:w-full sm:flex-col">
+          <div className="w-[60%] flex items-start justify-between gap-5 md:w-full sm:flex-col lg:w-[60%]">
             <ul className="w-[32%] !mt-1.5 flex flex-wrap justify-between gap-5 sm:w-full">
               <li>
                 <Link href="Terms" target="_blank" rel="noreferrer">
@@ -282,7 +283,7 @@ export default function Footer(props) {
                 src="/images/Linkedin.png"
                 width={38}
                 height={34}
-                alt="User Icon"
+                alt="User  Icon"
                 className="h-[34px]"
               />
               <Image
@@ -300,7 +301,7 @@ export default function Footer(props) {
                 className="h-[34px]"
               />
             </div>
-          </div>
+          </div>  
         </div>
       </div>
     </footer>
