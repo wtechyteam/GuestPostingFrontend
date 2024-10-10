3dbb1d0037de4e1704fa8872ae8883750828a8d4
WTechy <wtechy@gmail.com>
2024-10-10 11:34:03 +0530

    important updates

--- app/lists/Blocked.jsx
+++ app/lists/Blocked.jsx
@@ -4,6 +4,7 @@ import { Button } from "../common/Button";
 import SuccessModal from "./../common/SuccessModal";
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import SingleBlockedCard from "./../common/SingleBlockedCard";
 import {
   fetchAllProducts,
   fetchBlockedProducts,
@@ -57,7 +58,7 @@ export default function UserProfile3({
 
   const { blockedProducts, loading } = useSelector((state) => state.products);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
+  const [modalMessage, setModalMessage] = useState("");
   const [isHovered, setIsHovered] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
 
@@ -129,8 +130,6 @@ export default function UserProfile3({
       await dispatch(unblockProduct(productId));
       setModalMessage("Product Unblocked Successfully");
       setIsModalOpen(true);
-    
-    
     } catch (error) {
       console.error("Error Unblocking product:", error);
     }
@@ -156,319 +155,7 @@ export default function UserProfile3({
   if (blockedProducts.length === 1) {
     return (
       <>
-        {blockedProducts.map((product, index) => {
-          return (
-            <div
-              key={index}
-              className="flex flex-col items-center justify-center gap-3.5 px-4 mb-80 border-blue_gray-100_02 border border-solid bg-gray-10 w-[98%] rounded-[14px]"
-            >
-              <div className="mr-1.5 mt-1 flex items-center justify-between gap-5 self-stretch md:mr-0 md:flex-col">
-                <div className="flex flex-1 items-center justify-center md:self-stretch">
-                  <div className="relative inline-block">
-                    <Text
-                      size="textxl"
-                      as="p"
-                      className="text-indigo-a400 mr-[0.5rem]"
-                      onMouseEnter={() => setIsHovered(true)}
-                      onMouseLeave={() => setIsHovered(false)}
-                    >
-                      {product.URL || urlIsHiddenText}
-                    </Text>
-
-                    {isHovered && (
-                      <div className="absolute left-36 bottom-full mb-2 p-2 bg-adsy_com-black text-gray-10 rounded shadow-lg text-[10px] w-44 transform -translate-x-2">
-                        {description}
-                      </div>
-                    )}
-                  </div>
-
-                  <div className="flex self-end rounded-[8px] bg-gray-200 p-1.5">
-                    {!!contributorText ? (
-                      <Text
-                        size="textxs"
-                        as="p"
-                        className="text-adsy_com-black font-semibold text-[8.83px]"
-                      >
-                        {contributorText}
-                      </Text>
-                    ) : null}
-                  </div>
-                </div>
-                <div className="flex items-center">
-                  <Link href="/buyPost">
-                    <Button
-                      onClick={() => console.log("Buy Post clicked")}
-                      color="indigo_a400"
-                      size="md"
-                      className="min-w-[134px] rounded-xl font-bold text-white bg-indigo-a400 text-sm h-10 mb-[0.3rem]"
-                    >
-                      {buyPostButton}
-                    </Button>
-                  </Link>
-
-                  <Image
-                  src="/images/unlock.png"
-                  width={24}
-                  height={24}
-                  alt="Block"
-                  className="ml-4 h-[24px] w-[24px] cursor-pointer"
-                  onClick={() => handleUnblockProduct(product._id)}
-                />
-                 {isModalOpen && (
-                    <SuccessModal onClose={() => setIsModalOpen(false)}>
-                      <p>{modalMessage}</p>
-                    </SuccessModal>
-                  )}
-                </div>
-              </div>
-              <hr className="mt-[-0.8rem] border-gray-300 w-full" />
-              <div className="self-stretch">
-                <div className="flex flex-col gap-4">
-                  <div className="mr-3.5 flex items-start md:mr-0 md:flex-col">
-                    <div className="flex w-[16%] flex-col gap-4 md:w-full">
-                      <div className="flex flex-col items-start gap-0.5">
-                        <Text
-                          size="textlg"
-                          as="p"
-                          className="text-adsy_com-black text-[11.5px]"
-                        >
-                          {contentPlacementText}
-                        </Text>
-                        <Heading
-                          as="p"
-                          className="text-adsy_com-black font-bold text-[11.5px]"
-                        >
-                          {product.contentPlacement || priceText}
-                        </Heading>
-                      </div>
-                      <div className="flex flex-col items-start gap-0.5">
-                        <Text
-                          size="textlg"
-                          as="p"
-                          className="text-adsy_com-black font-bold text-[11.5px]"
-                        >
-                          {writingPlacementText}
-                        </Text>
-                        <Heading
-                          as="p"
-                          className="text-adsy_com-black font-bold text-[11.5px]"
-                        >
-                          {product.writingAndPlacement || writingPriceText}
-                        </Heading>
-                      </div>
-                    </div>
-
-                    <div className="border-l border-gray-300 h-full"></div>
-                    <div className="flex w-[20%] flex-col items-start md:w-full">
-                      <Text
-                        size="textlg"
-                        as="p"
-                        className="text-adsy_com-black text-[11.5px]"
-                      >
-                        {mozDAText}
-                      </Text>
-                      <Heading
-                        as="p"
-                        className="text-adsy_com-black font-bold text-[11.5px]"
-                      >
-                        {product.mozDA || mozDAValue}
-                      </Heading>
-                      <div className="mt-4 flex flex-col items-start gap-1 self-stretch">
-                        <Text
-                          size="textlg"
-                          as="p"
-                          className="text-adsy_com-black text-[11.5px]"
-                        >
-                          {semrushDAText}
-                        </Text>
-                        <Heading
-                          as="p"
-                          className="text-adsy_com-black font-bold text-[11.5px]"
-                        >
-                          {product.semrushDA || semrushDAValue}
-                        </Heading>
-                      </div>
-                      <div className="mt-4 flex flex-col items-start gap-0.5 self-stretch">
-                        <Text
-                          size="textlg"
-                          as="p"
-                          className="text-adsy_com-black text-[11.5px]"
-                        >
-                          {ahrefsDRRangeText}
-                        </Text>
-                        <Heading
-                          as="p"
-                          className="text-adsy_com-black font-bold text-[11.5px] mb-2"
-                        >
-                          {product.ahrefsDRrange || ahrefsDRRangeValue}
-                        </Heading>
-                      </div>
-                    </div>
-
-                    {/* <div className="border-l border-gray-300 h-full"></div>
-                    <div className="flex w-[16%] flex-col items-start md:w-full">
-                      <Text
-                        size="textlg"
-                        as="p"
-                        className="text-adsy_com-black text-[11.5px]"
-                      >
-                        {completionRateText}
-                      </Text>
-                      <Heading
-                        as="p"
-                        className="text-adsy_com-black font-bold text-[11.5px]"
-                      >
-                        {product.completionRate || completionRateValue}
-                      </Heading>
-                      <div className="mt-4 flex flex-col items-start gap-0.5 self-stretch">
-                        <Text
-                          size="textlg"
-                          as="p"
-                          className="text-adsy_com-black text-[11.5px]"
-                        >
-                          {avgLifetimeOfLinksText}
-                        </Text>
-                        <Heading
-                          as="p"
-                          className="text-adsy_com-black font-bold text-[11.5px]"
-                        >
-                          {product.avgLifetimeOfLinks ||
-                            avgLifetimeOfLinksValue}
-                        </Heading>
-                      </div>
-                      <div className="mt-4 flex flex-col items-start gap-0.5 self-stretch">
-                        <Text
-                          size="textlg"
-                          as="p"
-                          className="text-adsy_com-black text-[11.5px]"
-                        >
-                          {tatText}
-                        </Text>
-                        <Heading
-                          as="p"
-                          className="text-adsy_com-black font-bold text-[11.5px]"
-                        >
-                          {product.tat || tatValue}
-                        </Heading>
-                      </div>
-                      <div className="mt-4 flex flex-col items-start gap-0.5 self-stretch">
-                        <Text
-                          size="textlg"
-                          as="p"
-                          className="text-adsy_com-black text-[11.5px]"
-                        >
-                          {tasksWithInitialDomainText}
-                        </Text>
-                        <Heading
-                          as="p"
-                          className="text-adsy_com-black font-bold text-[11.5px]"
-                        >
-                          {product.tasksWithInitialDomain ||
-                            tasksWithInitialDomainValue}
-                        </Heading>
-                      </div>
-                    </div> */}
-
-                    <div className="border-l border-gray-300 h-full"></div>
-
-                    <div className="border-l border-gray-300 h-full"></div>
-                    <div className="flex w-[20%] flex-col items-start md:w-full">
-                      <Text
-                        size="textlg"
-                        as="p"
-                        className="text-adsy_com-black text-[11.5px]"
-                      >
-                        {ahrefsorganicTrafficText}
-                      </Text>
-                      <Heading
-                        as="p"
-                        className="text-adsy_com-black font-bold text-[11.5px]"
-                      >
-                        {product.ahrefsOrganicTraffic ||
-                          ahrefsOrganicTrafficValue}
-                      </Heading>
-                      <div className="mt-4 flex flex-col items-start gap-0.5 self-stretch">
-                        <Text
-                          size="textlg"
-                          as="p"
-                          className="text-adsy_com-black text-[11.5px]"
-                        >
-                          {totalTrafficText}
-                        </Text>
-                        <Heading
-                          as="p"
-                          className="text-adsy_com-black font-bold text-[11.5px]"
-                        >
-                          {product.totalTraffic || totalTrafficValue}
-                        </Heading>
-                      </div>
-                    </div>
-                    <div className="flex w-[20%] flex-col items-start md:w-full">
-                      <Text
-                        size="textlg"
-                        as="p"
-                        className="text-adsy_com-black text-[11.5px]"
-                      >
-                        {languageText}
-                      </Text>
-                      <Heading
-                        as="p"
-                        className="text-adsy_com-black font-bold text-[11.5px]"
-                      >
-                        {product.language || language}
-                      </Heading>
-                      <div className="mt-4 flex flex-col items-start gap-0.5 self-stretch">
-                        <Text
-                          size="textlg"
-                          as="p"
-                          className="text-adsy_com-black text-[11.5px]"
-                        >
-                          {countryText}
-                        </Text>
-                        <Heading
-                          as="p"
-                          className="text-adsy_com-black font-bold text-[11.5px]"
-                        >
-                          {product.country || countryValue}
-                        </Heading>
-                      </div>
-                    </div>
-                    <div className="flex w-[20%] flex-col items-start md:w-full">
-                      <Text
-                        size="textlg"
-                        as="p"
-                        className="text-adsy_com-black text-[11.5px]"
-                      >
-                        {linksText}
-                      </Text>
-                      <Heading
-                        as="p"
-                        className="text-adsy_com-black font-bold text-[11.5px]"
-                      >
-                        {product.links || linksValue}
-                      </Heading>
-                      <div className="mt-4 flex flex-col items-start gap-0.5 self-stretch">
-                        <Text
-                          size="textlg"
-                          as="p"
-                          className="text-adsy_com-black text-[11.5px]"
-                        >
-                          {markedSponsoredByText}
-                        </Text>
-                        <Heading
-                          as="p"
-                          className="text-adsy_com-black font-bold text-[11.5px]"
-                        >
-                          {product.markedSponsoredBy || markedSponsoredByValue}
-                        </Heading>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+        <SingleBlockedCard />
       </>
     );
   }
@@ -533,11 +220,11 @@ export default function UserProfile3({
                   className="ml-4 h-[24px] w-[24px] cursor-pointer"
                   onClick={() => handleUnblockProduct(product._id)}
                 />
-                 {isModalOpen && (
-                    <SuccessModal onClose={() => setIsModalOpen(false)}>
-                      <p>{modalMessage}</p>
-                    </SuccessModal>
-                  )}
+                {isModalOpen && (
+                  <SuccessModal onClose={() => setIsModalOpen(false)}>
+                    <p>{modalMessage}</p>
+                  </SuccessModal>
+                )}
               </div>
             </div>
             <hr className="mt-[-0.8rem] border-gray-300 w-full" />
@@ -623,69 +310,6 @@ export default function UserProfile3({
                       </Heading>
                     </div>
                   </div>
-
-                  {/* <div className="border-l border-gray-300 h-full"></div>
-                  <div className="flex w-[16%] flex-col items-start md:w-full">
-                    <Text
-                      size="textlg"
-                      as="p"
-                      className="text-adsy_com-black text-[11.5px]"
-                    >
-                      {completionRateText}
-                    </Text>
-                    <Heading
-                      as="p"
-                      className="text-adsy_com-black font-bold text-[11.5px]"
-                    >
-                      {product.completionRate || completionRateValue}
-                    </Heading>
-                    <div className="mt-4 flex flex-col items-start gap-0.5 self-stretch">
-                      <Text
-                        size="textlg"
-                        as="p"
-                        className="text-adsy_com-black text-[11.5px]"
-                      >
-                        {avgLifetimeOfLinksText}
-                      </Text>
-                      <Heading
-                        as="p"
-                        className="text-adsy_com-black font-bold text-[11.5px]"
-                      >
-                        {product.avgLifetimeOfLinks || avgLifetimeOfLinksValue}
-                      </Heading>
-                    </div>
-                    <div className="mt-4 flex flex-col items-start gap-0.5 self-stretch">
-                      <Text
-                        size="textlg"
-                        as="p"
-                        className="text-adsy_com-black text-[11.5px]"
-                      >
-                        {tatText}
-                      </Text>
-                      <Heading
-                        as="p"
-                        className="text-adsy_com-black font-bold text-[11.5px]"
-                      >
-                        {product.tat || tatValue}
-                      </Heading>
-                    </div>
-                    <div className="mt-4 flex flex-col items-start gap-0.5 self-stretch">
-                      <Text
-                        size="textlg"
-                        as="p"
-                        className="text-adsy_com-black text-[11.5px]"
-                      >
-                        {tasksWithInitialDomainText}
-                      </Text>
-                      <Heading
-                        as="p"
-                        className="text-adsy_com-black font-bold text-[11.5px]"
-                      >
-                        {product.tasksWithInitialDomain ||
-                          tasksWithInitialDomainValue}
-                      </Heading>
-                    </div>
-                  </div> */}
 
                   <div className="border-l border-gray-300 h-full"></div>
 
--- app/redux/productSlice.js
+++ app/redux/productSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import Cookies from "js-cookie";
- 
+
 // Async thunk to fetch all products
 export const fetchAllProducts = createAsyncThunk(
   "products/fetchAll",
@@ -10,7 +10,7 @@ export const fetchAllProducts = createAsyncThunk(
     return response.data;
   }
 );
- 
+
 // Async thunk to fetch blocked products for the current user
 export const fetchBlockedProducts = createAsyncThunk(
   "products/fetchBlocked",
@@ -25,7 +25,7 @@ export const fetchBlockedProducts = createAsyncThunk(
     return response.data;
   }
 );
- 
+
 // Async thunk to fetch unblocked products for the current user
 export const fetchUnblockedProducts = createAsyncThunk(
   "products/fetchUnblocked",
@@ -34,13 +34,13 @@ export const fetchUnblockedProducts = createAsyncThunk(
     const response = await axios.get(
       "http://localhost:3001/api/unblockedProducts",
       {
-        headers: { Authorization: `Bearer ${token}`},
+        headers: { Authorization: `Bearer ${token}` },
       }
     );
     return response.data;
   }
 );
- 
+
 // Async thunk to block a product
 export const blockProduct = createAsyncThunk(
   "products/block",
@@ -60,13 +60,13 @@ export const blockProduct = createAsyncThunk(
     }
   }
 );
- 
+
 // Async thunk to unblock a product
 export const unblockProduct = createAsyncThunk(
   "products/unblock",
   async (productId, { rejectWithValue }) => {
     const token = Cookies.get("authToken");
-    
+
     try {
       const response = await axios.post(
         `http://localhost:3001/api/unblock/${productId}`,
@@ -82,9 +82,7 @@ export const unblockProduct = createAsyncThunk(
     }
   }
 );
- 
- 
- 
+
 // Initial state
 const initialState = {
   products: [],
@@ -93,7 +91,7 @@ const initialState = {
   loading: false,
   error: null,
 };
- 
+
 // Create product slice
 const productSlice = createSlice({
   name: "products",
@@ -123,47 +121,56 @@ const productSlice = createSlice({
       .addCase(blockProduct.fulfilled, (state, action) => {
         // Ensure that action.payload contains just the productId
         const productId = action.payload; // Assuming payload is just the ID
-    
-        console.log('Blocking product:', productId);
-        console.log('Current products before removal:', state.products);
-    
+
+        console.log("Blocking product:", productId);
+        console.log("Current products before removal:", state.products);
+
         // Remove blocked product from products list
-        state.products = state.products.filter((product) => product._id !== productId);
-    
-        console.log('Updated products after removal:', state.products);
-    
+        state.products = state.products.filter(
+          (product) => product._id !== productId
+        );
+
+        console.log("Updated products after removal:", state.products);
+
         // Ensure blockedProducts is an array before pushing
         if (!Array.isArray(state.blockedProducts)) {
-            console.error('blockedProducts is not an array. Initializing as an empty array.');
-            state.blockedProducts = [];
+          console.error(
+            "blockedProducts is not an array. Initializing as an empty array."
+          );
+          state.blockedProducts = [];
         }
-    
+
         // Push the productId into blockedProducts
         state.blockedProducts.push(productId);
-    })
-    
-    
-    .addCase(unblockProduct.fulfilled, (state, action) => {
-      const productId = action.payload;
- 
-      // Remove the product from blockedProducts
-      state.blockedProducts = state.blockedProducts.filter((id) => id !== productId);
- 
-      // Retrieve the original product from allProducts and add it back to products
-      const unblockedProduct = state.products.find((product) => product._id === productId);
-      if (unblockedProduct) {
-        state.products.push(unblockedProduct);
-      } else {
-        console.warn(`Unblocked product with ID ${productId} not found in allProducts.`);
-      }
-    })
-    .addCase(blockProduct.rejected, (state, action) => {
-      state.error = action.payload;
-    })
-    .addCase(unblockProduct.rejected, (state, action) => {
-      state.error = action.payload;
-    });
+      })
+
+      .addCase(unblockProduct.fulfilled, (state, action) => {
+        const productId = action.payload;
+
+        // Remove the product from blockedProducts
+        state.blockedProducts = state.blockedProducts.filter(
+          (id) => id !== productId
+        );
+
+        // Retrieve the original product from allProducts and add it back to products
+        const unblockedProduct = state.products.find(
+          (product) => product._id === productId
+        );
+        if (unblockedProduct) {
+          state.products.push(unblockedProduct);
+        } else {
+          console.warn(
+            `Unblocked product with ID ${productId} not found in allProducts.`
+          );
+        }
+      })
+      .addCase(blockProduct.rejected, (state, action) => {
+        state.error = action.payload;
+      })
+      .addCase(unblockProduct.rejected, (state, action) => {
+        state.error = action.payload;
+      });
   },
 });
- 
+
 export default productSlice.reducer;
