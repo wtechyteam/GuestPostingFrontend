30e6b4e4b0b2d246d9fa733fe4fefbc8e9d56541
Wtechy <randeep@wtmails.com>
2024-10-10 12:25:39 +0530

    store comments

--- app/redux/store.js
+++ app/redux/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './productSlice';
 import wishlistReducer from './wishlistSlice'
  
+
+//store to maintain all the reducers and actions
 const store = configureStore({
   reducer: {
     products: productReducer,
