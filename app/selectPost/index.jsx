13a51270f8258e74dbf9cb908697d28f54a5a36b
WTechy <wtechy@gmail.com>
2024-10-15 18:14:07 +0530

    updates

--- app/selectPost/index.jsx
+++ app/selectPost/index.jsx
@@ -106,22 +106,22 @@ export default function SelectPostsPage() {
       dateText: "12 Aug, 2024",
     },
   ];
-
   return (
-    <div className="w-full bg-gray-10 overflow-x-hidden">
-      <div className="flex flex-col items-end bg-gray-50_01">
-        <header className="fixed top-0 left-0 w-[100%] z-50 flex items-center justify-center bg-gray-10 p-1.5 shadow-6xl md:w-full">
-          <div className="mb-1 flex w-[96%] items-center justify-between md:w-full sm:flex-col">
-            <div className="flex w-[100%] flex-col items-start sm:w-full">
-              <Image
-                src="/images/logo.png"
-                width={56}
-                height={32}
-                alt="Sidebar Logo"
-                className="ml-6 h-[32px] w-[56px] object-contain"
-              />
-            </div>
-            <div className="flex w-full flex-col items-start sm:w-full ml-[-65rem]">
+    <>
+      <div className="w-full bg-gray-10 overflow-x-hidden">
+        <div className="flex flex-col items-end bg-gray-50_01">
+          <header className="fixed top-0 left-0 w-full z-50 flex items-center justify-center bg-gray-10 p-1.5 shadow-6xl">
+            <div className="mb-1 flex w-[96%] items-center justify-between">
+              <div className="flex flex-col items-start">
+                <Image
+                  src="/images/logo.png"
+                  width={56}
+                  height={32}
+                  alt="Sidebar Logo"
+                  className="ml-6 h-[32px] w-[56px] object-contain"
+                />
+              </div>
+              <div className="flex w-[40%] mr-[100px] flex-col items-start sm:w-full  md:ml-0 sm:ml-0">
               <Heading
                 size="heading2xl"
                 as="h6"
@@ -136,7 +136,7 @@ export default function SelectPostsPage() {
               </div>
             </div>
 
-            <div className="flex w-[18%] items-center justify-center gap-4 sm:w-full">
+            <div className="flex w-[18%]  ml-4 mr-[-1rem] items-center justify-center gap-4 sm:w-full">
               <SelectBox
                 color="gray_50"
                 size="sm"
@@ -193,12 +193,18 @@ export default function SelectPostsPage() {
                 </div>
 
                 {isDropdownVisible && (
-                  <div className="absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg z-50">
+                  <div className="absolute right-0 mt-2 w-48 bg-gray-10 border rounded-lg shadow-lg z-50">
                     <button
                       onClick={handleLogout2}
                       className="block w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100"
                     >
-                      Profile Details
+                      Profile Settings
+                    </button>
+                    <button
+                      onClick={handleLogout}
+                      className="block w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100"
+                    >
+                      Switch To Seller
                     </button>
                     <button
                       onClick={handleLogout}
@@ -210,19 +216,19 @@ export default function SelectPostsPage() {
                 )}
               </div>
             </div>
-          </div>
-        </header>
+            </div>
+          </header>
 
-        <div className="flex items-start justify-start gap-7 self-stretch">
-          <div className="fixed top-100 mt-[50px] left-0 h-full bg-white shadow-md z-20">
-            <Sidebar1 />
-          </div>
-          <div className="ml-[300px] mt-[100px] flex flex-1 flex-col gap-7">
-            <HowItWorks
-              isHowItWorksVisible={isHowItWorksVisible}
-              toggleHowItWorks={toggleHowItWorks}
-            />
-            <h1 className="text-3xl font-bold text-gray-700 !text-left">
+          <div className="flex items-start justify-start gap-7 self-stretch mt-[70px]">
+            <div className="fixed top-[60px] left-0 h-full bg-white shadow-md z-20">
+              <Sidebar1 />
+            </div>
+            <div className="ml-[300px] mt-[30px] flex flex-1 flex-col gap-7">
+              <HowItWorks
+                isHowItWorksVisible={isHowItWorksVisible}
+                toggleHowItWorks={toggleHowItWorks}
+              />
+              <h1 className="text-3xl font-bold text-gray-700 !text-left">
               Create a Task
             </h1>
 
@@ -534,9 +540,11 @@ export default function SelectPostsPage() {
                 )}
               </div>
             </div>
+            
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
