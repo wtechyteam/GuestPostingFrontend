9cffb177ae8feb45b0a132b812e39bb61b44a6e9
WTechy <wtechy@gmail.com>
2024-10-22 17:20:14 +0530

    a change

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -86,7 +86,7 @@ export default function LoginOnePage() {
   };
 
   return (
-    <div className="w-full h-auto bg-gray-10 mb-[20rem] flex flex-col overflow-hidden">
+    <div className="w-full h-auto bg-gray-10  flex flex-col overflow-hidden">
       <div className="mt-8 flex flex-col items-start gap-28 md:gap-[84px] sm:gap-14">
         <Link href="/">
           <Text
@@ -98,176 +98,202 @@ export default function LoginOnePage() {
           </Text>
         </Link>
         <div className="flex items-start self-stretch md:flex-col">
-          <div className="mt-[92px] flex-1 self-end md:self-stretch md:px-5 sm:self-auto">
-            <div className="flex items-start md:flex-col">
-            <Image
-  src="/images/Saly-14.png"
-  width={262} // Adjust the size here
-  height={468} // Adjust the size here
-  alt="Saly Image"
-  className="mt-[20px] h-auto w-[36%] self-end object-contain block md:hidden lg:w-[36%]" // Hidden on small/medium screens, visible on large screens
-/>
 
 
-<div className="flex flex-1 flex-col items-start px-1.5 md:self-stretch sm:items-center sm:justify-center md:items-center md:justify-center">
 
-  <Heading
-    size="heading7xl"
-    as="h2"
-    className="!text-indigo-a400 !text-[48px] font-bold sm:text-[42px] text-center md:text-center"
-  >
-    Login in to
-  </Heading>
 
-  <Heading
-    size="text9xl"
-    as="h3"
-    className="!text-gray-10 mt-16 !text-[34.28px] sm:!text-[30px] text-center md:text-left"
-  >
-    <span className="text-adsy_com-black">Generate&nbsp;</span>
-  </Heading>
 
-  <div className="relative h-[72px] self-stretch">
-    <div className="absolute bottom-0 right-[10%] top-0 my-auto h-[60px] w-[64%] bg-indigo-a400" />
-    <div className="absolute left-0 right-0 top-[3.47px] m-auto flex flex-1 flex-col items-start">
+          
+        <div className="mt-[92px] flex-1 self-end md:self-stretch md:px-5 sm:self-auto">
+  <div className="flex items-start md:flex-col sm:items-center sm:justify-center">
+    <Image
+      src="/images/Saly-14.png"
+      width={262} // Adjust the size here
+      height={468} // Adjust the size here
+      alt="Saly Image"
+      className="mt-[20px] h-auto w-[36%] self-end object-contain block md:hidden lg:w-[36%]" // Visible on medium screens and above
+    />
+
+    <div className="flex flex-1 flex-col items-start px-1.5 md:self-stretch sm:items-center sm:justify-center">
       <Heading
-        size="text13xl"
-        as="h4"
-        className="!text-[46.16px] sm:!text-[35px] md:!text-[40px] !text-gray-10 text-center md:text-left"
+        size="heading7xl"
+        as="h2"
+        className="!text-indigo-a400 !text-[48px] font-bold sm:text-[42px] text-center md:text-center"
       >
-        <span className="text-adsy_com-black">More</span>
-        <span className="text-gray-10 opacity-30 -z-10 transform translate-x-6">
-          &nbsp;
-        </span>
-        <span className="font-bold underline">
-          {displayedWord}
-        </span>
+        Login in to
       </Heading>
-    </div>
-  </div>
 
-  <Text
-    as="p"
-    className="ml-1.5 mt-[68px] !font-poppins !text-adsy_com-black md:ml-0 text-center md:text-left"
-  >
-    If you do not have an account register
-  </Text>
-  <Link href="/signup">
-    <Text
-      as="p"
-      className="ml-1.5 mt-1.5 !font-poppins !text-adsy_com-black md:ml-0 text-center md:text-left"
-    >
-      <span className="text-adsy_com-black">You can &nbsp;</span>
-      <span className="font-semibold text-indigo-a400">
-        Sign Up here !
-      </span>
-    </Text>
-  </Link>
-</div>
-
-</div>
-          </div>
-          <div className="flex w-[44%] flex-col items-center px-14 md:w-full md:px-5 md:items-center">
-  <Text
-    size="text7xl"
-    as="p"
-    className="self-start !font-poppins !font-medium !text-adsy_com-black text-[22px] sm:text-[28px] text-center md:text-left"
-  >
-    Log In
-  </Text>
-  <form
-    onSubmit={handleLogin}
-    className="mt-6 flex w-full flex-col items-start md:items-center"
-  >
-    <Input
-      shape="round"
-      type="email"
-      name="Email or Phone Input"
-      placeholder="  Enter Email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-      className="w-[60%] h-[60px] bg-[#E7ECFF] text-[#3861FB] placeholder-[#3861FB] font-poppins md:w-[80%]"
-      style={{ paddingLeft: "1rem" }}
-    />
-    <Input
-      color="blue 50"
-      shape="round"
-      type={isPasswordVisible ? "text" : "password"}
-      name="Password Input"
-      placeholder="  Password"
-      suffix={
-        <Image
-          src={
-            isPasswordVisible
-              ? "/images/Frame 43967.png"
-              : "/images/invisible.png"
-          }
-          width={16}
-          height={16}
-          alt="Visibility Icon"
-          className="h-[16px] w-[16px] mr-[1rem] cursor-pointer"
-          onClick={togglePasswordVisibility}
-        />
-      }
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-      className="w-[60%] h-[60px] bg-[#E7ECFF] text-[#3861FB] placeholder-[#3861FB] font-poppins mt-[1rem] md:w-[80%]"
-      style={{ paddingLeft: "1rem" }}
-    />
-    {errorMessage && (
-      <Text as="p" className="text-red-500 mt-2">
-        {errorMessage}
-      </Text>
-    )}
-    <div className="mt-[20px] flex w-[82%] flex-col items-end gap-[18px] md:w-full">
-      <Text
-        size="textsm"
-        as="p"
-        className="!font-poppins !font-normal !text-gray-400 text-xs mr-[10rem] md:mr-[5rem] sm:ml-[-15rem]"
+      <Heading
+        size="text9xl"
+        as="h3"
+        className="!text-gray-10 mt-16 !text-[34.28px] sm:!text-[30px] text-center md:text-left"
       >
-        Forgot password ?
-      </Text>
-    </div>
+        <span className="text-adsy_com-black">Generate&nbsp;</span>
+      </Heading>
 
-    <Button
-      color="indigo_A400"
-      size="2xl"
-      className="mt-6 w-[60%] rounded-lg font-poppins font-medium text-white shadow-lg hover:shadow-xl transition-shadow duration-300 md:w-[80%]"
-      style={{ backgroundColor: "#3861FB" }}
-    >
-      Login
-    </Button>
-    <Link href="#" className="mt-[2rem] md:ml-[0]">
-      <Text as="p" className="!font-poppins !font-medium !text-gray-400 items-center justify-center">
-        or continue with
+      <div className="relative h-[72px] self-stretch sm:items-center sm:justify-center">
+        <div className="absolute bottom-0 right-[10%] top-0 my-auto h-[60px] w-[64%] bg-indigo-a400 sm:right-auto sm:left-0 sm:mx-auto sm:w-[80%]" />
+        <div className="absolute left-0 right-0 top-[3.47px] m-auto flex flex-1 flex-col items-start sm:items-center">
+          <Heading
+            size="text13xl"
+            as="h4"
+            className="!text-[46.16px] sm:!text-[35px] md:!text-[40px] !text-gray-10 text-center md:text-left"
+          >
+            <span className="text-adsy_com-black">More</span>
+            <span className="text-gray-10 opacity-30 -z-10 transform translate-x-6 sm:hidden">
+              &nbsp;
+            </span>
+            <span className="font-bold underline">{displayedWord}</span>
+          </Heading>
+        </div>
+      </div>
+
+      <Text
+        as="p"
+        className="ml-1.5 mt-[68px] !font-poppins !text-adsy_com-black md:ml-0 text-center md:text-left"
+      >
+        If you do not have an account register
       </Text>
-    </Link>
-  </form>
-  <div className="mt-6 flex flex-row items-center justify-center w-full gap-[3rem] mr-[6rem]  sm:ml-[12rem] sm:justify-center">
-    <Image
-      src="/images/Facebook2.png"
-      width={40}
-      height={40}
-      alt="Facebook Icon"
-      className="h-[40px] w-[40px] object-cover"
-    />
-    <Image
-      src="/images/apple.png"
-      width={40}
-      height={40}
-      alt="Apple Icon"
-      className="h-[40px] w-[40px] object-cover"
-    />
-    <Image
-      src="/images/google.png"
-      width={40}
-      height={40}
-      alt="Google Icon"
-      className="h-[40px] w-[40px] object-cover"
-    />
+      <Link href="/signup">
+        <Text
+          as="p"
+          className="ml-1.5 mt-1.5 !font-poppins !text-adsy_com-black md:ml-0 text-center md:text-left"
+        >
+          <span className="text-adsy_com-black">You can &nbsp;</span>
+          <span className="font-semibold text-indigo-a400">
+            Sign Up here!
+          </span>
+        </Text>
+      </Link>
+    </div>
   </div>
 </div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <div className="flex w-[44%] flex-col items-center px-14 md:w-full md:px-5 md:items-center">
+            <Text
+              size="text7xl"
+              as="p"
+              className="self-start !font-poppins !font-medium !text-adsy_com-black text-[22px] sm:text-[28px] text-center md:text-left"
+            >
+              Log In
+            </Text>
+            <form
+              onSubmit={handleLogin}
+              className="mt-6 flex w-full flex-col items-start md:items-center"
+            >
+              <Input
+                shape="round"
+                type="email"
+                name="Email or Phone Input"
+                placeholder="  Enter Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-[60%] h-[60px] bg-[#E7ECFF] text-[#3861FB] placeholder-[#3861FB] font-poppins md:w-[80%]"
+                style={{ paddingLeft: "1rem" }}
+              />
+              <Input
+                color="blue 50"
+                shape="round"
+                type={isPasswordVisible ? "text" : "password"}
+                name="Password Input"
+                placeholder="  Password"
+                suffix={
+                  <Image
+                    src={
+                      isPasswordVisible
+                        ? "/images/Frame 43967.png"
+                        : "/images/invisible.png"
+                    }
+                    width={16}
+                    height={16}
+                    alt="Visibility Icon"
+                    className="h-[16px] w-[16px] mr-[1rem] cursor-pointer"
+                    onClick={togglePasswordVisibility}
+                  />
+                }
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-[60%] h-[60px] bg-[#E7ECFF] text-[#3861FB] placeholder-[#3861FB] font-poppins mt-[1rem] md:w-[80%]"
+                style={{ paddingLeft: "1rem" }}
+              />
+              {errorMessage && (
+                <Text as="p" className="text-red-500 mt-2">
+                  {errorMessage}
+                </Text>
+              )}
+              <div className="mt-[20px] flex w-[82%] flex-col items-end gap-[18px] md:w-full">
+                <Text
+                  size="textsm"
+                  as="p"
+                  className="!font-poppins !font-normal !text-gray-400 text-xs mr-[10rem] md:mr-[5rem] sm:ml-[-15rem]"
+                >
+                  Forgot password ?
+                </Text>
+              </div>
+
+              <Button
+                color="indigo_A400"
+                size="2xl"
+                className="mt-6 w-[60%] rounded-lg font-poppins font-medium text-white shadow-lg hover:shadow-xl transition-shadow duration-300 md:w-[80%]"
+                style={{ backgroundColor: "#3861FB" }}
+              >
+                Login
+              </Button>
+              <Link href="#" className="mt-[2rem] md:ml-[0]">
+                <Text
+                  as="p"
+                  className="!font-poppins !font-medium !text-gray-400 items-center justify-center"
+                >
+                  or continue with
+                </Text>
+              </Link>
+            </form>
+            <div className="mt-6 flex flex-row items-center justify-center w-full gap-[3rem] mr-[6rem]  sm:ml-[12rem] sm:justify-center">
+              <Image
+                src="/images/Facebook2.png"
+                width={40}
+                height={40}
+                alt="Facebook Icon"
+                className="h-[40px] w-[40px] object-cover"
+              />
+              <Image
+                src="/images/apple.png"
+                width={40}
+                height={40}
+                alt="Apple Icon"
+                className="h-[40px] w-[40px] object-cover"
+              />
+              <Image
+                src="/images/google.png"
+                width={40}
+                height={40}
+                alt="Google Icon"
+                className="h-[40px] w-[40px] object-cover"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
