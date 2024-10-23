97bbc943df472bdb2c4f5498e3d6af39d985da6f
WTechy <wtechy@gmail.com>
2024-10-23 11:32:48 +0530

    fxing forgot password

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -240,12 +240,14 @@ export default function LoginOnePage() {
                 Login
               </Button>
               <Link href="#" className="mt-[2rem]">
-                <Text
-                  as="p"
-                  className="!font-poppins !font-medium !text-gray-400 items-center justify-center text-center"
-                >
-                  or continue with
-                </Text>
+              <Text
+  as="p"
+  className="!font-poppins !font-medium !text-gray-400 flex md:items-center md:justify-center md:ml-[8rem] sm:items-center sm:justify-center  sm:ml-0 text-center  ml-[10rem]"
+>
+  or continue with
+</Text>
+
+
               </Link>
             </form>
             <div className="mt-6 flex flex-row items-center justify-center w-full gap-[3rem] sm:gap-[2rem] sm:w-[100%]">
